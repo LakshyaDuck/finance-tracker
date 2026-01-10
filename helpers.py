3ae6a09adb58b2108edd12cb54a7a681e95a7a5b
sainilakshya <saini.lakshya.official@gmail.com>
2026-01-10 22:01:58 +0530

    Remove redundent functions from helpers

--- helpers.py
+++ helpers.py
@@ -1,8 +1,5 @@
-import requests
-
 from flask import redirect, render_template, session
 from functools import wraps
-from sqlalchemy import select
 
 def login_required(f):
     @wraps(f)
@@ -21,21 +18,3 @@ def apology(message, code=400):
 def usd(value):
     """Format value as USD."""
     return f"${value:,.2f}"
-
-
-def get_user_by_id():
-    return session.get("user_id")
-
-
-def get_account_with_verification():
-    user_id = get_user_by_id()
-    if not user_id:
-        return None
-    account_id = session.get("account_id")
-    if account_id is None:
-        return None
-    a_ids = select(Account.id).where(Account.user_id == user_id)
-    if account_id not in a_ids:
-        return None
-    return account_id
-
